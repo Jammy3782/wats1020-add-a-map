1be7b20926dd999f420964719d454e584be5ce50
Jammy3782 <jammy3782@gmail.com>
2017-02-26 17:14:41 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,14 +9,14 @@
 
 // TODO: Inside of your on ready handler, invoke the Leaflet.js library
 // to draw a map in your `#map-container` div.
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([46.7853, -121.7350], 10);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+L.marker([46.7853, -121.7350]).addTo(map)
+    .bindPopup('See me at the top!')
     .openPopup();
 // TODO: Add 2 layers to your map you have visuals. Use the Open Street Maps
 // tiles served through the MapQuest CDN. Consult this example to set up
@@ -26,5 +26,3 @@ L.marker([51.5, -0.09]).addTo(map)
 // TODO: Customize that Map to show markers with popups at no fewer than 3
 // interesting locations. (You'll need to figure out the latitude/longitude for
 // these locations using a mapping tool such as Google Maps.)
-
-
